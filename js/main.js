69e8d711b81727327db281ac3360f97e982bbafb
MSR1000 <msr@MSRs-MBP.home>
2015-09-01 09:54:26 -0400

    force ha?

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 $( document ).ready(function() {
 
 $(window).scroll(function() {
-  var theta = ($(window).scrollTop() % Math.PI)/5;
-  $('#left-gear').css({ transformOrigin: '50% 50%', transform: 'rotate(' + theta + 'rad)' });
-  $('#center-gear').css({ transformOrigin: '50% 50%', transform: 'rotate(-' + theta + 'rad)' });
- $('#right-gear').css({ transformOrigin: '50% 50%', transform: 'rotate(' + theta + 'rad)' });
+  var theta = ($(window).scrollTop() % Math.PI)/2;
+  $('#left-gear').css({ transformOrigin: '50% 50% 0px', transform: 'rotate(' + theta + 'rad)' });
+  $('#center-gear').css({ transformOrigin: '50% 50% 0px', transform: 'rotate(-' + theta + 'rad)' });
+ $('#right-gear').css({ transformOrigin: '50% 50% 0px', transform: 'rotate(' + theta + 'rad)' });
 });
 
 
