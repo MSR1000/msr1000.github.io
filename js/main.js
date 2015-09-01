af03ee6f58f97a7f5b92de2e468a05ef4d14dcd2
MSR1000 <msr@MSRs-MBP.home>
2015-09-01 09:45:11 -0400

    oh shit they rotate!
    silly circles!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-console.log('Script started');
 
-window.onload = function(){
- console.log('Window loaded');
+$( document ).ready(function() {
 
-}
+$(window).scroll(function() {
+  var theta = ($(window).scrollTop() % Math.PI)/5;
+  $('#left-gear').css({ transformOrigin: '50% 50%', transform: 'rotate(' + theta + 'rad)' });
+  $('#center-gear').css({ transformOrigin: '50% 50%', transform: 'rotate(-' + theta + 'rad)' });
+ $('#right-gear').css({ transformOrigin: '50% 50%', transform: 'rotate(' + theta + 'rad)' });
+});
+
+
+
+
+});
+
